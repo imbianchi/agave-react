f1d00f292a1469cce739d5674d7048450f6e0ca5
local <joaobianchi@live.com>
2021-06-03 20:27:03 -0300

    :construction: created home/invite pages

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="container">
+            <div className="row d-flex justify-content-center">
+                <div className="col-12 mt-3 w-25">
+                    <img src="/logo-preto.png" alt="Vercel Logo" width="100%" height="100%" />
+                </div>
+                <h1 className="d-flex justify-content-center mt-4">
+                    Em breve!
+                    </h1>
+            </div>
+        </div >
+    );
+}
+
+export default Home;
---
+++ src/pages/Invite/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactPlayer from 'react-player/youtube';
+
+function Invite() {
+    document.body.style = 'background: #A7A7A7;';
+
+    return (
+        <div className="container">
+            <div className="row">
+
+                <div className="col-12 mb-5">
+                    <div className="videoWrapper">
+                        <ReactPlayer width="100%" height="100%" url="https://www.youtube.com/watch?v=ScMzIvxBSi4" />
+                    </div>
+                    <div className="videoDecoration" />
+                </div>
+
+                <div className="col-12 d-flex flex-column justify-content-center align-items-center">
+                    <img className="logo" src="/logo-preto.png" />
+                    <div className="separator" />
+                    <div className="address">
+                        <p>AV. Itororó, 1079</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Invite;
