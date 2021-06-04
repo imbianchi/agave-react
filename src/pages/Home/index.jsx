acad58f67dd8742e8f9880ac28d6c4fd7dece648
local <joaobianchi@live.com>
2021-06-03 21:04:34 -0300

    :lipstick: text uppercase home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,9 +7,9 @@ function Home() {
                 <div className="col-12 mt-3 w-25">
                     <img src="/logo-preto.png" alt="Vercel Logo" width="100%" height="100%" />
                 </div>
-                <h1 className="d-flex justify-content-center mt-4">
+                <h1 className="d-flex justify-content-center mt-4 text-uppercase">
                     Em breve!
-                    </h1>
+                </h1>
             </div>
         </div >
     );
