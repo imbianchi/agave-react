0241a083cfb7c526379aaffa6cac2ba042851230
Gabriel Gonçalves <gabriel_r_goncalves@hotmail.com>
2022-01-27 19:31:57 -0300

    create Home style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import style from './style.scss';
 
 const Home = () => {
   return (
-    <div>
+    <div className='container'>
       <h1>Home</h1>
       <p>Conteúdo da página</p>
-    </div>
+    </div >
   )
 }
 
