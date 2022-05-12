a2d9869438e0f16df7da85f4aad1a9e47c7572e1
Gabriel Souza <dev.gabrielsouza@gmail.com>
2022-05-12 17:57:05 -0300

    Change buttons href and rebuild projet

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () => {
       <Switch>
         <BrowserRouter>
           <Route exact path="/" component={() => <Redirect to='/premioagave' />} />
-          <Route path="/register" component={Register} />
-          <Route path="/rule" component={Rule} />
+          <Route path="/premioagave/register" component={Register} />
+          <Route path="/premioagave/rule" component={Rule} />
           <Route path="/premioagave" component={Jurors} />
         </BrowserRouter>
       </Switch>
