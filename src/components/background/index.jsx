ce563976922c9d1030e258f626374bb633f6cd4c
Gabriel Souza <dev.gabrielsouza@gmail.com>
2022-05-16 16:14:00 -0300

    :bento: add sofa

--- src/components/background/index.jsx
+++ src/components/background/index.jsx
@@ -4,7 +4,7 @@ const Background = () => {
     <div className='background'>
       <img className='part1' src='/assets/ring.png' />
       <img className='part2' src='/assets/chair.png' />
-      {/* <img className='part3' src='/assets/sofa.png' /> */}
+      <img className='part3' src='/assets/sofa.png' />
     </div>
   )
 }
